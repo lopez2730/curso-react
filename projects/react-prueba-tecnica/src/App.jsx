8736bba069d96b5b6ebef258b9be16910efa6963
lopez2730 <all_30@lvie.com>
2024-10-14 15:32:57 -0600

    prueba realizada, falta testing

---
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com';
+
+export function App() {
+  const [ fact, setFact ] = useState();
+  const [ imageUrl, setImageUrl ] = useState();
+
+  useEffect( () => {
+    fetch( CAT_ENDPOINT_RANDOM_FACT )
+      .then( res => res.json() )
+      .then( data => {
+        const { fact } = data;
+        setFact( fact );
+      } );
+  }, [] );
+
+
+  useEffect( () => {
+    if ( !fact ) return;
+    const threeFirstWords = fact.split( ' ', 3 ).join( ' ' );
+    fetch( `https://cataas.com/cat/says/${ threeFirstWords }?size=10&color=red&json=true` )
+      .then( res => res.json() )
+      .then( response => {
+        const { _id } = response;
+        const url = `/cat/${ _id }/says/${ threeFirstWords }`;
+        setImageUrl( url );
+      } );
+  }, [ fact ] );
+
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      { fact && <p>{ fact }</p> }
+      { imageUrl && <img src={ `${ CAT_PREFIX_IMAGE_URL }${ imageUrl }` } alt={ `Image extracted using the first trhee words for ${ fact }` } /> }
+    </main>
+  );
+}
